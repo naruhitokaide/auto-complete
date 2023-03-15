eea27b40425f7f9ceca85246e0a8f2c5a811cb81
naruhitokaide <naruhitokaide@gmail.com>
2023-03-15 18:34:07 +0800

    Add test case and followed the requirements.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,106 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders input box", () => {
+    render(<App />);
+    const inputElement = screen.getByRole("textbox", { name: "Search" });
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test("shows loading spinner when API call is made", async () => {
+    const mockData = { total_count: 0, incomplete_results: false, items: [] };
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      } as Response)
+    );
+    render(<App />);
+    const inputElement = screen.getByRole("textbox", { name: "Search" });
+    await act(async () => {
+      userEvent.type(inputElement, "naruhito");
+    });
+    const loadingElement = await screen.findByText("Loading...");
+    expect(loadingElement).toBeInTheDocument();
+  });
+
+  test("renders error message when API call fails", async () => {
+    jest
+      .spyOn(global, "fetch")
+      .mockImplementation(() => Promise.reject(new Error("API call failed")));
+    render(<App />);
+    const inputElement = screen.getByRole("textbox", { name: "Search" });
+    await act(async () => {
+      userEvent.type(inputElement, "naruhito");
+    });
+    const errorElement = await screen.findByText(
+      "An error occurred. Please try again later."
+    );
+    expect(errorElement).toBeInTheDocument();
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test("renders list of items when API call succeeds", async () => {
+    const mockData = {
+      total_count: 1,
+      incomplete_results: false,
+      items: [{ login: "naruhitokaide", id: 1, avatar_url: "", html_url: "" }],
+    };
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      } as Response)
+    );
+    render(<App />);
+    const inputElement = screen.getByRole("textbox", { name: "Search" });
+    await act(async () => {
+      userEvent.type(inputElement, "naruhito");
+    });
+    const itemElement = await screen.findByText("kaide");
+    expect(itemElement).toBeInTheDocument();
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test("renders error message when no items found", async () => {
+    const mockData = {
+      total_count: 0,
+      incomplete_results: false,
+      items: [],
+    };
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      } as Response)
+    );
+    render(<App />);
+    const inputElement = screen.getByRole("textbox", { name: "Search" });
+    await act(async () => {
+      userEvent.type(inputElement, "naruhito");
+    });
+    const errorElement = await screen.findByText("No results found.");
+    expect(errorElement).toBeInTheDocument();
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test("renders highlighted items when input changes", async () => {
+    const mockData = {
+      total_count: 1,
+      incomplete_results: false,
+      items: [
+        { login: "naruhitokaide", id: 123, avatar_url: "", html_url: "" },
+      ],
+    };
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      } as Response)
+    );
+    render(<App />);
+    const inputElement = screen.getByRole("textbox", { name: "Search" });
+    await act(async () => {
+      userEvent.type(inputElement, "naruhito");
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import "./App.css";
-import { debounce } from "lodash";
 
 function App() {
+  // Define the types of the GitHub API response data
+  interface GitHubUser {
+    login: string;
+    id: number;
+    avatar_url: string;
+    html_url: string;
+  }
+
+  interface GitHubSearchResult {
+    total_count: number;
+    incomplete_results: boolean;
+    items: GitHubUser[];
+  }
+
   // State variables to keep track of the user's input and the list of items to display
   const [query, setQuery] = useState<string>("");
   const [itemList, setItemList] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
   // Event handler to update the query state variable whenever the user types in the input box
   const onQueryChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,42 +29,71 @@ function App() {
 
   // Effect hook to make API calls to the GitHub API whenever the user's query changes
   useEffect(() => {
-    // Use Lodash's debounce function to delay API calls and prevent spamming the API
-    const delayApiCall = debounce(() => {
-      if (query) {
-        axios
-          .get(`https://api.github.com/search/users?q=${query}`)
-          .then((res) => {
-            const data = res.data;
-            // Check if the response contains any items and update the itemList state variable
+    // Create a variable to hold the timeout ID
+    let timeout: NodeJS.Timeout;
+
+    // Only make API calls if the query is not empty
+    if (query) {
+      // Show the loading spinner while the API call is being made
+      setLoading(true);
+      // Set a timeout to avoid making too many API calls while the user is typing
+      timeout = setTimeout(() => {
+        // Make the API call to the GitHub API
+        fetch(`https://api.github.com/search/users?q=${query}`)
+          .then((res) => res.json())
+          .then((data: GitHubSearchResult) => {
+            setLoading(false);
+            // If the API response has items, update the itemList state variable
             if (data?.items?.length) {
+              // Only show the first 10 items and extract the login names
               setItemList(
-                data?.items?.slice(0, 10)?.map((item: any) => item.login) || []
+                data?.items
+                  ?.slice(0, 10)
+                  ?.map((item: GitHubUser) => item.login) || []
               );
+            } else {
+              // If the API response doesn't have any items, show an error message
+              setItemList([]);
+              setError("No results found.");
             }
           })
           .catch((error) => {
+            // If an error occurs, show an error message
+            setLoading(false);
+            setError("An error occurred. Please try again later.");
             console.log(error);
-            // Handle errors here
           });
-      }
-    }, 300);
+      }, 300);
+    } else {
+      // If the query is empty, reset the itemList and error state variables
+      setItemList([]);
+      setError("");
+    }
 
-    // Call the debounced API function
-    delayApiCall();
-
-    // Return a cleanup function to cancel any pending API calls when the query changes
-    return delayApiCall.cancel;
+    // Return a cleanup function to clear the timeout when the component unmounts or the query changes
+    return () => clearTimeout(timeout);
   }, [query]);
 
   // Render the input box and the list of items
   return (
     <div className="container">
       <h1> Type the github username that you want to list.</h1>
-      <input type="text" name="query" value={query} onChange={onQueryChange} />
+      <input
+        type="text"
+        name="query"
+        value={query}
+        onChange={onQueryChange}
+        aria-label="Search"
+      />
+      {/* Show the loading spinner while the API call is being made */}
+      {loading && <p>Loading...</p>}
+      {/* Show the error message if an error occurs */}
+      {error && <p>{error}</p>}
       <ul>
+        {/* Only show the itemList if it is not empty */}
         {!!itemList.length &&
           itemList.map((item) => {
+            // Create a regular expression to highlight the user's query in the item name
             const regex = new RegExp(query, "gi");
             // Use regex to highlight the user's query in the item name
             const highlightedItem = item.replace(
